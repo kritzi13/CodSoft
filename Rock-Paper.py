bccdf7cd6cef6f1b6d1aae5b5345ae89ce9b5ce6
Kratika Gupta <201377216+kritzi13@users.noreply.github.com>
2025-07-07 16:43:04 +0530

    Update Rock-Paper.py

--- Rock-Paper.py
+++ Rock-Paper.py
@@ -1,1 +1,8 @@
+import random
+options=("Rock", "Paper", "Scissors")
+computer = random.choice(options)
 
+player = input ("Enter a choice (Rock, Paper, Scissors): ")
+
+print(f"Player: {player}")
+print(f"Computer: {computer}")
